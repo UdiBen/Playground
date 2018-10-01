f68812e7d072e0b76d17d62f0f0c150b08795431
Udi Ben Amitai <udi.b@taboola.com>
2018-10-02 00:07:22 +0300

    wire it all

--- src/component/App.js
+++ src/component/App.js
@@ -1,14 +1,9 @@
 import React, {Component} from 'react';
 import cat from '../cat.png';
-import Button from "./Button.js";
 import WeatherGraph from "./WeatherGraph.js";
 import './App.css';
 
 class App extends Component {
-
-    handleClick = () => {
-       // WeatherGraph.load
-    }
 
     render() {
         return (
@@ -16,7 +11,6 @@ class App extends Component {
                 <header className="App-header">
                     <img src={cat} className="App-logo" alt="logo"/>
                     <h1 className="app-title">When will my hat fly?!</h1>
-                    <Button name="Find out" clickHandler={this.handleClick} />
                 </header>
                 <WeatherGraph></WeatherGraph>
             </div>
--- src/component/WeatherGraph.js
+++ src/component/WeatherGraph.js
@@ -1,15 +1,70 @@
 import React, {Component} from 'react';
-import { Chart } from "react-google-charts";
+import {Chart} from "react-google-charts";
+import moment from 'moment';
 
 class WeatherGraph extends Component {
+    constructor() {
+        super();
+        this.state = {
+            weatherData: []
+        }
+    }
+
+    componentDidMount() {
+        function fix(weatherData) {
+            let maxVal = weatherData.reduce((max, p) => p[1] > max ? p[1] : max, weatherData[0][1]);
+
+            let numbers = weatherData.map(item => item.concat(item[1] === maxVal
+                ? 'point { size: 18; shape-type: star; fill-color: #a52714; visible:true; }'
+                : null));
+
+            numbers.unshift(["Hour", "Wind (mph)", {'type': 'string', 'role': 'style'}]);
+            return numbers;
+        }
+
+        fetch('https://api2.climacell.co/v2/historical', {
+            method: 'post',
+            headers: new Headers({
+                'apikey': 'S984VQLZqIKmB7bCS6YFaV9sRUBAtYY7',
+                'Content-Type': 'application/json'
+            }),
+            body: JSON.stringify({
+                "geocode": {
+                    "lon": -71.303273,
+                    "lat": 44.270587
+                },
+                "location_id": "",
+                "start_time": "2018-03-19T00:00:00Z",
+                "end_time": "2018-03-19T23:59:59Z",
+                "timestep": 60,
+                "fields": [
+                    {
+                        "name": "temp",
+                        "units": "F"
+                    },
+                    {
+                        "name": "wind_speed",
+                        "units": "mph"
+                    }
+                ]
+            })
+        }).then(results => results.json())
+            .then(result => {
+                let data = result.map(x=> [ this.formatTime(x), x.wind_speed.value]);
+                this.setState({weatherData: fix(data)});
+            });
+    }
+
+    formatTime(x) {
+        return moment(x.observation_time.value).format("HH:MM:ss");
+    }
+
     render() {
         return (
             <div className={"wind-chart"}>
                 <Chart
                     chartType="LineChart"
-                    data={[["Date", "Wind", {'type': 'string', 'role': 'style'}],
-                        ["Sep 28", 5.5, null],
-                        ["Sep 29", 12, 'point { size: 18; shape-type: star; fill-color: #a52714; visible:true; }']]}
+                    data={this.state.weatherData}
                     width="100%"
                     height="400px"
                     legendToggle
